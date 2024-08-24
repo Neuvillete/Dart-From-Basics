efcca2f2d5120c53cc8b5089fbe1a45f31df4b03
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-08-25 01:23:20 +0530

    Create leapyear_Q9.dart

---
+++ Beginner/Solutions/Basic Syntax and Control Flow/leapyear_Q9.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a year:');
+  int year = int.parse(stdin.readLineSync()!);
+
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    print('$year is a leap year.');
+  } 
+  else {
+    print('$year is not a leap year.');
+  }
+}
