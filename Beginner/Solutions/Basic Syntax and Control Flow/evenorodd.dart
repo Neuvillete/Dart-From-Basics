bb790cc33bb834085608b4294d22f440d8e12fcd
Aditya Chowdhury <143079032+Neuvillete@users.noreply.github.com>
2024-08-24 17:48:36 +0530

    Create evenorodd.dart

---
+++ Beginner/Solutions/Basic Syntax and Control Flow/evenorodd.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a number:');
+  int number = int.parse(stdin.readLineSync()!);
+
+  if (number % 2 == 0) {
+    print('$number is even.');
+  } 
+  else {
+    print('$number is odd.');
+  }
+}
